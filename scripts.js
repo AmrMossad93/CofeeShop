5604997175a38083a455bd0b07634b7494d5cddd
AmrMossad93 <eng.amrmossad93@gmail.com>
2019-10-30 01:25:27 +0200

    Updates

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$('.menu').on('click', function () {
+    $(this).toggleClass('active');
+    $('.overlay').toggleClass('menu-open');
+});
+$('.nav a').on('click', function () {
+    $('.menu').toggleClass('active');
+    $('.overlay').toggleClass('menu-open');
+});
